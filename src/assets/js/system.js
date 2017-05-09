5f29b28bd59ed55a558c5bb1fff2d5f62d043d3d
jiuxuan <117898271@qq.com>
2017-05-09 18:25:31 +0800

    提取公共js

--- src/assets/js/system.js
+++ src/assets/js/system.js
@@ -1,7 +1,51 @@
-var header = {//导航相关
+var common = {//公共js
     init:function () {
-        header.filter();  //搜索下拉
-        header.dropMenu();  //非遗名录下拉
+        header.init();
+        this.top();
+        this.input();
+    },
+    top: function () {//返回顶部
+        $('.gotop').click(function () {
+            $("html,body").animate({scrollTop: 0}, 500);
+        });
+    },
+    input: function () {
+        //输入框特效
+        var _form = $('.form');
+        var _input = _form.find('input.ipt');
+        _input.each(function () {
+            var _this = $(this);
+
+            //初始化val值
+            var oldVal = _this.val();
+
+            //获取焦点
+            _this.focus(function () {
+                // clearInterval(timer);
+                $(this).val('');
+            });
+
+            //失去焦点
+            _this.blur(function () {
+                // clearInterval(timer);
+
+                var newVal = $(this).val();
+                if (newVal == '') {
+                    $(this).val(oldVal);
+                }
+
+                // timer = setInterval(slider, speed);
+            });
+
+        });
+    },
+
+};
+
+var header = {//导航相关
+    init: function () {
+        this.filter();  //搜索下拉
+        this.dropMenu();  //非遗名录下拉
     },
     filter: function () {
         var _header = $('.header');
@@ -40,7 +84,7 @@ var header = {//导航相关
         });
 
     },
-    dropMenu:function () {//非遗名录下拉
+    dropMenu: function () {//非遗名录下拉
         var drop = $('.drop_menu');
         var item = drop.find('.item');
         var _header = $('.header');
@@ -57,26 +101,26 @@ var header = {//导航相关
 
 
         //去掉dd的左边距
-        item.eq(0).css('width','82px');
-        item.eq(1).css('width','143px');
-        item.eq(2).css('width','100px');
-        item.eq(3).css('width','240px');
+        item.eq(0).css('width', '82px');
+        item.eq(1).css('width', '143px');
+        item.eq(2).css('width', '100px');
+        item.eq(3).css('width', '240px');
 
-        item.eq(3).find('a:even').css('width','72px');
-        item.eq(3).find('a:odd').css({'width':'129px','margin-left':'24px'});
+        item.eq(3).find('a:even').css('width', '72px');
+        item.eq(3).find('a:odd').css({'width': '129px', 'margin-left': '24px'});
 
-        item.eq(4).css('width','210px');
-        item.eq(4).find('a:even').css('width','115px');
-        item.eq(4).find('a:odd').css({'width':'66px','margin-left':'24px'});
+        item.eq(4).css('width', '210px');
+        item.eq(4).find('a:even').css('width', '115px');
+        item.eq(4).find('a:odd').css({'width': '66px', 'margin-left': '24px'});
 
     },
 };
 
 var homePage = {
     init: function () {
-        homePage.slide(); //轮播图
-        homePage.input();  //输入框
-        homePage.scroll();  //滑动导航
+        this.slide(); //轮播图
+        // this.input();  //输入框
+        this.scroll();  //滑动导航
     },
     slide: function () {//轮播图
         var parent = $('.slider');
@@ -118,42 +162,6 @@ var homePage = {
 
 
     },
-    input: function () {
-        //输入框特效
-        var _form = $('.form');
-        var _input = _form.find('input.ipt');
-        _input.each(function () {
-            var _this = $(this);
-
-            //初始化val值
-            var oldVal = _this.val();
-
-            //获取焦点
-            _this.focus(function () {
-                // clearInterval(timer);
-                $(this).val('');
-            });
-
-            //失去焦点
-            _this.blur(function () {
-                // clearInterval(timer);
-
-                var newVal = $(this).val();
-                if (newVal == '') {
-                    $(this).val(oldVal);
-                }
-
-                // timer = setInterval(slider, speed);
-            });
-
-
-        });
-    },
-    top: function () {//返回顶部
-        $('.gotop').click(function () {
-            $("html,body").animate({scrollTop: 0}, 500);
-        });
-    },
     scroll: function () {//滚动
         var _header = $('#home-header');
         var _top = $('.gotop');
@@ -175,6 +183,7 @@ var homePage = {
 $(function () {
     homePage.init();
     header.init();
+    common.init();
 });
 
 
