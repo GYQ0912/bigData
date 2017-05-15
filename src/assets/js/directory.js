eb903c31b829b0575f67d22347fc678cfc0bae87
jiuxuan <117898271@qq.com>
2017-05-15 10:46:30 +0800

    添加素材

---
+++ src/assets/js/directory.js
@@ -0,0 +1,49 @@
+(function () {
+	var page = {
+		controller: function(){
+			page.bind();
+			page.init();
+		},
+
+		init: function(){
+			//导航
+			// var _header = $('.header')
+	  //       var _icon =_header.find('.content .info li.search')
+
+	  //       $(window).scroll(function () {
+	  //           if ($(window).scrollTop() > 100) {
+	  //               _header.addClass('active');
+	  //               _icon.show();
+	  //           } else {
+	  //               _header.removeClass('active');
+	  //               _icon.hide();
+	  //           }
+
+	  //       });
+		},
+
+		bind: function(){
+			var load_more = document.querySelectorAll(".load_more");
+			for(var i = 0; i < load_more.length; i++){
+				load_more[i].addEventListener("click", function(){
+					page.loadMore(this);
+				}, false);
+			}
+		},
+
+		//Load More
+		loadMore: function(element){
+			var classify = element.getAttribute("data-classify");
+
+			element.innerHTML = "Loading...";
+			var timer = setTimeout(function(){
+				var list = document.querySelector("."+classify).innerHTML;
+				console.log(list);
+				list += list;
+				document.querySelector("."+classify).innerHTML = list;
+				element.innerHTML = "Load More";
+			},3000);
+		}
+	}
+	page.controller();
+})()
