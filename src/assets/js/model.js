922fde1371764003335c61d17cfdbb5ca48b74e7
efeiyi <efeiyi@AppledeMacBook-Pro.local>
2017-05-25 16:21:45 +0800

    css

--- src/assets/js/model.js
+++ src/assets/js/model.js
@@ -1,6 +1,9 @@
 var examplePage = {
     init: function () {
         this.mainBg();
+
+        this.share();
+        this.praise();
     },
     mainBg: function () {//首屏图片 视频
         var obj=$('.bg');
@@ -11,6 +14,64 @@ var examplePage = {
         _img.css({
             'margin-top': -parseInt(imgH / 2) + 'px',
             'margin-left': -parseInt(imgW / 2) + 'px',
+        });
+
+    },
+    share: function () { //分享
+        var obj=$('.base .main .floor');
+
+        obj.each(function () {
+            var _share = $(this).find('a.share');
+            var _shareBox = $(this).find('.share_box');
+            //弹出框
+            _share.on('click', function () {
+                _shareBox.stop(true).fadeToggle();
+                return false;
+            });
+
+            _shareBox.on('click', function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            $(document).on("click", function () {
+                _shareBox.fadeOut();
+            });
+        });
+    },
+    praise: function () { //点赞功能
+        var obj=$('.base .main .floor');
+        var praise = obj.find('.praise');
+        praise.on('click', function () {
+            var _this = $(this);
+
+            //创建动画数字
+            if (_this.hasClass('active')) { //取消点赞
+                _this.removeClass('active');
+                _this.append('<div class="add"><b>-1</b></div>');
+                animateNum('.add');
+            } else { //点赞
+                _this.addClass('active');
+                _this.append('<div class="add"><b>+1</b></div>');
+                animateNum('.add');
+
+            }
+
+            function animateNum(obj) {
+                $(obj).css({
+                    'position': 'absolute',
+                    'z-index': '1',
+                    'color': '#C30',
+                    'left': '5px',
+                    'top': '-15px',
+                }).animate({
+                    left: 15,
+                    top: -30
+                }, 'slow', function () {
+                    $(this).fadeIn('fast').remove();
+                });
+            }
+
         });
 
     },
